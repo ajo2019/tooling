f3e8913890fcb43d4743e0007bb3386040fc283c
kayode adebayo <kaybabaguv20023@gmail.com>
2025-10-08 01:33:28 +0100

    opdate on the s3 bucket

--- modules/s3-dynamodb/variables.tf
+++ modules/s3-dynamodb/variables.tf
@@ -1,7 +1,7 @@
 variable "bucket" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "class388-terraform-backend-bucket"
+  default     = "class390-terraform-backend-bucket"
 }
 
 variable "table" {
